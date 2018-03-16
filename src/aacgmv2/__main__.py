28a30566d1be984962ca0139a965076c9c498701
Stefan Bender <stefan.bender@ntnu.no>
2018-03-16 15:35:13 +0100

    aacgm: Fix command line file output
    Just passing argparse.FileType() resulted in empty output files when
    used in np.savetxt(). Wrapping it in `with ... as f:` seems to mitigate
    this issue and produces correct output files.

--- src/aacgmv2/__main__.py
+++ src/aacgmv2/__main__.py
@@ -68,11 +68,13 @@ def main():
         date = dt.date.today() if args.date is None else dt.datetime.strptime(args.date, '%Y%m%d')
         lats, lons = aacgmv2.convert(array[:, 0], array[:, 1], array[:, 2], date=date, a2g=args.a2g, trace=args.trace,
                                      allowtrace=args.allowtrace, badidea=args.badidea, geocentric=args.geocentric)
-        np.savetxt(args.file_out, np.column_stack((lats, lons)), fmt='%.8f')
+        with args.file_out as f:
+            np.savetxt(args.file_out, np.column_stack((lats, lons)), fmt='%.8f')
     elif args.subcommand == 'convert_mlt':
         datetime = dt.datetime.strptime(args.datetime, '%Y%m%d%H%M%S')
         out = aacgmv2.convert_mlt(array, datetime, m2a=args.m2a)
-        np.savetxt(args.file_out, out, fmt='%.8f')
+        with args.file_out as f:
+            np.savetxt(args.file_out, out, fmt='%.8f')
 
 
 if __name__ == '__main__':
