30da4a46f3cfacc8855e46f8181dcf3085dcbe16
Stefan Bender <stefan.bender@ntnu.no>
2018-04-02 20:38:44 +0200

    Try to fix undefined math constants on Windows
    M_PI is a POSIX extension which needs _USE_MATH_DEFINES to be defined
    when including <math.h> on Windows. Detected by builds failing on
    AppVeyor, hopefully fixing them.
    
    (Some) References: robotology/QA#81
    https://stackoverflow.com/questions/1727881/how-to-use-the-pi-constant-in-c

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
                       'src/c_aacgm_v2/igrflib.c',
                       'src/c_aacgm_v2/mlt_v2.c',
                       'src/c_aacgm_v2/rtime.c'],
+                  extra_compile_args=['-D_USE_MATH_DEFINES'],
                   include_dirs=['src/c_aacgm_v2'])
     ],
     entry_points={
