eafc90c24870b5cb221e58c855a48b49772dca7b
Stefan Bender <stefan.bender@ntnu.no>
2018-05-26 20:38:15 +0200

    setup: Remove unused `relpath` import
    This module was not used.

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
-from os.path import relpath
 from os.path import splitext
 
 from setuptools import find_packages
